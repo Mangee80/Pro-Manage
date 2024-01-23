0435dc8a53480be50a02e71fef6c1c9e44b0439c
Mani Pratap Singh <sDemanipratap@gmail.com>
2024-01-23 16:58:05 +0530

    Dashboard structure

---
+++ client/src/Components/Home/Analytics/Analytics.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Style.module.css';
+
+export const Analytics = () => {
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.h1}></h1>
+        
+    </div>
+  )
+}
